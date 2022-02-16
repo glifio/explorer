522384053b4cef153f2f95301f1a22cf9b894fbf
Thijs van Hoof <thijs.v.h@gmail.com>
2022-02-16 10:28:07 +0100

    Pass app header hrefs to environment variables

--- next.config.js
+++ next.config.js
@@ -22,36 +22,39 @@ const webpack = (config) => {
 }
 
 module.exports = (phase) => {
-  if (phase === PHASE_PRODUCTION_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    return {
-      trailingSlash: true,
-      webpack,
-      env: {
-        // this api is configured to be load balanced across multiple nodes,
-        // if a single node gets sick, it will get dropped and not accept requests
-        NEXT_PUBLIC_LOTUS_NODE_JSONRPC:
-          process.env.LOTUS_NODE_JSONRPC || 'https://api.node.glif.io',
-        NEXT_PUBLIC_GRAPH_API_URL:
-          process.env.GRAPH_API_URL || 'graph.glif.host/query',
-        // 461'
-        NEXT_PUBLIC_COIN_TYPE: process.env.COIN_TYPE || 'f',
-        NEXT_PUBLIC_IS_PROD: true,
-
-        NEXT_PUBLIC_SENTRY_DSN: process.env.SENTRY_DSN || '',
-        NEXT_PUBLIC_SENTRY_ENV: process.env.SENTRY_ENV || ''
-      }
-    }
-  }
   return {
     trailingSlash: true,
     webpack,
     env: {
-      NEXT_PUBLIC_LOTUS_NODE_JSONRPC:
-        process.env.LOTUS_NODE_JSONRPC || 'https://calibration.node.glif.io',
       NEXT_PUBLIC_GRAPH_API_URL:
         process.env.GRAPH_API_URL || 'graph.glif.host/query',
-      // 1'
-      NEXT_PUBLIC_COIN_TYPE: process.env.COIN_TYPE || 't'
+      NEXT_PUBLIC_WALLET_HREF:
+        process.env.WALLET_HREF || 'https://calibration.wallet.glif.io',
+      NEXT_PUBLIC_SAFE_HREF:
+        process.env.SAFE_HREF || 'https://calibration.safe.glif.io',
+      NEXT_PUBLIC_EXPLORER_HREF:
+        process.env.EXPLORER_HREF || 'https://calibration.explorer.glif.io',
+      NEXT_PUBLIC_VERIFIER_HREF:
+        process.env.VERIFIER_HREF || 'https://verify.glif.io',
+      NEXT_PUBLIC_SENTRY_DSN: process.env.SENTRY_DSN || '',
+      NEXT_PUBLIC_SENTRY_ENV: process.env.SENTRY_ENV || '',
+
+      ...(phase === PHASE_PRODUCTION_SERVER || phase === PHASE_PRODUCTION_BUILD
+        ? {
+            // this api is configured to be load balanced across multiple nodes,
+            // if a single node gets sick, it will get dropped and not accept requests
+            NEXT_PUBLIC_LOTUS_NODE_JSONRPC:
+              process.env.LOTUS_NODE_JSONRPC || 'https://api.node.glif.io',
+            NEXT_PUBLIC_COIN_TYPE: process.env.COIN_TYPE || 'f', // 461'
+            NEXT_PUBLIC_IS_PROD: true
+          }
+        : {
+            NEXT_PUBLIC_LOTUS_NODE_JSONRPC:
+              process.env.LOTUS_NODE_JSONRPC ||
+              'https://calibration.node.glif.io',
+            NEXT_PUBLIC_COIN_TYPE: process.env.COIN_TYPE || 't', // 1'
+            NEXT_PUBLIC_IS_PROD: false
+          })
     }
   }
 }
