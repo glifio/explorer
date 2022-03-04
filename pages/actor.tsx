3dade883a7a07ef1cb7aac5ad51d41a1f33c5776
Thijs van Hoof <thijs.v.h@gmail.com>
2022-03-04 10:47:59 +0100

    Create ExplorerPage and apply on actor and message view

--- pages/actor.tsx
+++ pages/actor.tsx
@@ -1,12 +1,12 @@
 import {
   ActorState,
   MessageHistoryTable,
-  Page,
   OneColumn
 } from '@glif/react-components'
 import { validateAddressString } from '@glif/filecoin-address'
 import { useRouter } from 'next/router'
 import { PAGE } from '../constants'
+import ExplorerPage from '../src/components/ExplorerPage'
 import SearchBar from '../src/components/SearchBar'
 
 export default function Actor() {
@@ -15,12 +15,7 @@ export default function Actor() {
   const isString = typeof address === 'string'
   const validAddress = isString && validateAddressString(address)
   return (
-    <Page
-      homeUrl={process.env.NEXT_PUBLIC_HOME_URL}
-      blogUrl={process.env.NEXT_PUBLIC_BLOG_URL}
-      walletUrl={process.env.NEXT_PUBLIC_WALLET_URL}
-      safeUrl={process.env.NEXT_PUBLIC_SAFE_URL}
-    >
+    <ExplorerPage>
       {!validAddress && (
         <OneColumn>
           <h2>
@@ -54,6 +49,6 @@ export default function Actor() {
           </OneColumn>
         </>
       )}
-    </Page>
+    </ExplorerPage>
   )
 }
---
+++ src/components/ExplorerPage.tsx
@@ -0,0 +1,31 @@
+import { Page, PageProps, ExplorerIconHeaderFooter } from '@glif/react-components'
+
+export default function ExplorerPage({ children, ...rest }: PageProps) {
+  return (
+    <Page
+      appIcon={<ExplorerIconHeaderFooter />}
+      appUrl='/'
+      appHeaderLinks={[
+        {
+          title: 'Blog',
+          url: process.env.NEXT_PUBLIC_BLOG_URL
+        },
+        {
+          title: 'Wallet',
+          url: process.env.NEXT_PUBLIC_WALLET_URL
+        },
+        {
+          title: 'Safe',
+          url: process.env.NEXT_PUBLIC_SAFE_URL
+        }
+      ]}
+      {...rest}
+    >
+      {children}
+    </Page>
+  )
+}
+
+ExplorerPage.propTypes = {
+  ...Page.propTypes
+}
