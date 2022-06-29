a532d69cea21c17e3f8fdff2e806d4917251419d
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-29 11:18:53 +0200

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,4 @@
 const { TextDecoder, TextEncoder } = require('util')
-
 global.TextDecoder = TextDecoder
 global.TextEncoder = TextEncoder
 
@@ -7,6 +6,14 @@ import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import 'whatwg-fetch'
 
+process.env.NEXT_PUBLIC_HOME_URL = 'https://glif.io'
+process.env.NEXT_PUBLIC_BLOG_URL = 'https://blog.glif.io/'
+process.env.NEXT_PUBLIC_WALLET_URL = 'https://wallet-calibration.glif.link'
+process.env.NEXT_PUBLIC_SAFE_URL = 'https://safe-calibration.glif.link'
+process.env.NEXT_PUBLIC_EXPLORER_URL = 'https://explorer-calibration.glif.link'
+process.env.NEXT_PUBLIC_VERIFIER_URL = 'https://verify-calibration.glif.link'
 process.env.NEXT_PUBLIC_LOTUS_NODE_JSONRPC =
   'https://calibration.node.glif.io/rpc/v0'
 process.env.NEXT_PUBLIC_COIN_TYPE = 't'
+process.env.NEXT_PUBLIC_GRAPH_API_URL =
+  'https://graph-calibration.glif.link/query'
