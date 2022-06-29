eb2c819e1708cfd2f3b07183bf6eeae23c4ed0ce
Thijs van Hoof <thijs.v.h@gmail.com>
2022-06-29 11:19:01 +0200

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -5,4 +5,5 @@ export enum PAGE {
   MESSAGE = '/message'
 }
 
-export const GLIF_DISCORD = 'https://discord.gg/8Hqm3qvK'
+export const GLIF_DISCORD = 'https://discord.gg/B9ju5Eu4Rq'
+export const GLIF_TWITTER = 'https://twitter.com/glifio'
