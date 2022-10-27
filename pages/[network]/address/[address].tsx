c9b0402d61bd63cbc48a152cad20729ef8fe947c
Jon <Schwartz10@users.noreply.github.com>
2022-10-27 19:33:54 -0400

    Add new pages for handling network as a path param (#146)

---
+++ pages/[network]/address/[address].tsx
@@ -0,0 +1,8 @@
+import { useRouter } from 'next/router'
+import { AddressView } from '../../../src/components/AddressView'
+
+export default function Actor() {
+  const router = useRouter()
+  const address = router.query.address as string
+  return <AddressView address={address} />
+}
